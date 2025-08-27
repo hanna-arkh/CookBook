44aa396ef2c13355b6063645f3eed2d24a56663b
hanna-arkh <hanna.arkhipenka@innowise.com>
2025-08-27 16:23:10 +0200

    add basic registration screen with email validation

---
+++ CookBook/store/store.tsx
@@ -0,0 +1,43 @@
+import { create } from 'zustand'
+
+type AuthState = {
+  isLoggedIn: boolean
+  userToken: string | null
+  isLoading: boolean
+  error: string | null
+  signIn: (username: string, password: string) => Promise<void>
+  register: (email: string, password: string) => Promise<void>
+}
+
+export const useAuthStore = create<AuthState>(set => ({
+  isLoggedIn: false,
+  userToken: null,
+  isLoading: false,
+  error: null,
+
+  signIn: async (username, password) => {
+    set({ isLoading: true, error: null })
+    try {
+      if (username === 'test@example.com' && password === 'password') {
+        set({ userToken: 'fake-token', isLoggedIn: true })
+      } else {
+        set({ error: 'Invalid email or password', isLoggedIn: false })
+      }
+    } finally {
+      set({ isLoading: false })
+    }
+  },
+
+  register: async (email, password) => {
+    set({ isLoading: true, error: null })
+    try {
+      if (email && password) {
+        set({ userToken: 'fake-token', isLoggedIn: true })
+      } else {
+        set({ error: 'Fill all inputs', isLoggedIn: false })
+      }
+    } finally {
+      set({ isLoading: false })
+    }
+  },
+}))
--- CookBook/types/types.ts
+++ CookBook/types/types.ts
@@ -1,5 +1,20 @@
 export type Recipe = {
-  strMeal: string;
-  strMealThumb: string;
-  idMeal: string;
-};
+  strMeal: string
+  strMealThumb: string
+  idMeal: string
+}
+export type PropsEmail = {
+  value: string
+  onChangeText: (text: string) => void
+  isValid: boolean
+}
+export type PropsPassword = {
+  value: string
+  onChangeText: (text: string) => void
+}
+
+export type ButtonRegisterProps = {
+  onPress: () => void
+  isLoading: boolean
+  disabled: boolean
+}
