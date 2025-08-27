82203a6f95a6fc016f6694db81dfdfc299563add
hanna-arkh <hanna.arkhipenka@innowise.com>
2025-08-27 14:05:00 +0200

    add search recipes input

--- CookBook/app/(tabs)/index.tsx
+++ CookBook/app/(tabs)/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
+import { SafeAreaView, StyleSheet, Text, View, TextInput } from 'react-native'
 import RecipesItem from '@/components/RecipesItem'
 import { FlatList } from 'react-native'
 import { Recipe } from '@/types/types'
 import { fetchRecipes } from '@/services/api/recipes'
+import { COLORS } from '@/constants/Colors'
 
 export default function ListOfRecipes() {
   const [recipes, setRecipes] = useState<Recipe[]>([])
   const [loading, setLoading] = useState(true)
+  const [searchQuery, setSearchQuery] = useState('')
 
+  const filteredRecipes = useMemo(() => {
+    if (!searchQuery) {
+      return recipes
+    }
+    return recipes.filter(recipe =>
+      recipe.strMeal.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  }, [recipes, searchQuery])
   useEffect(() => {
     const getRecipes = async () => {
       try {
@@ -30,10 +39,17 @@ export default function ListOfRecipes() {
       </View>
     )
   }
+
   return (
     <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search"
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+      />
       <FlatList
-        data={recipes}
+        data={filteredRecipes}
         keyExtractor={item => item.idMeal}
         renderItem={({ item }) => <RecipesItem item={item} />}
       />
@@ -49,6 +65,16 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     marginVertical: 30,
+  },
+  searchInput: {
+    height: 40,
+    borderColor: COLORS.GREY,
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginHorizontal: 16,
+    marginBottom: 10,
+    marginTop: 15,
   },
   separator: {
     marginVertical: 30,
